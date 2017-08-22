4c0254c0a79381cb5b46f7c4d2c4f71dcc68af5d
ServiceDog <smitham42@gmail.com>
2017-08-21 20:35:45 -0700

    xbox_guidance node created; transmits -1, 0, 1, unknown why not in between

--- src/xbox_guidance.cpp
+++ src/xbox_guidance.cpp
@@ -1,13 +1,80 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/Float32MultiArray.h"
 #include "sensor_msgs/Joy.h"
+#include <vector>
 
 #include <sstream>
 
+/*
+0: 
+1:
+2:
+3: [DRIVE] Turn intensity (1 to -1)
+4: [DRIVE] Velocity intensity (1 to -1)
+5:
 
-void joyCallback(const sensor_msgs::Joy) {
+...
+
+*/
+std::vector<float> controls(23,0.0);
+
+void joyCallback(const sensor_msgs::Joy::ConstPtr& msg) {
 
 
+/*
+
+Indexes of axes in msg->data.axes
+0: Left Stick (LS) LeftRight 
+1: Left Stick (LS) UpDown 
+2: Left Trigger (LT)
+3: Right Stick (RS) LeftRight 
+4: Right Stick (RS) UpDown 
+5: Right Trigger (RT)
+6: D-Pad (DP) LeftRight 
+7: D-Pad (DP) UpDown
+
+Indexes of buttons in msg->data.buttons
+0: A
+1: B
+2: X
+3: Y
+4: Left Bumper (LB)
+5: Right Bumper (RB)
+6: BACK Button
+7: START Button
+8: XBOX Button
+9: LS Click
+10: RS Click
+11: DP Left
+12: DP Right
+13: DP Up
+14: DP Down
+
+
+Axis values range from 1.0 to -1.0:
+
+For the D-Pad, 0 is unpressed, -1 is down/right, 1 is up/left
+
+For the triggers, 1 is unpressed, -1 is fully pressed
+
+For the sticks, -1 is down/right, 1 is up/left, 0 is untouched
+
+*/
+
+  // Right Stick
+  float lr, ud;
+  lr=msg->axes[3];
+  ud=msg->axes[4];
+ 
+  // Right Stick Safe Zones
+  lr=(abs(lr)<0.1)?0:lr;
+  ud=(abs(ud)<0.1)?0:ud;
+
+  // Set Controls
+  controls[3]=lr;
+  controls[4]=ud;
 }
 
 /**
@@ -19,9 +86,22 @@ int main(int argc, char **argv) {
   ros::NodeHandle n;
 
   ros::Subscriber sub = n.subscribe("joy", 1000, joyCallback);
-  ros::Publisher pub = n.advertise<std_msgs::String>("xbox_guidance", 1000);
+  ros::Publisher pub = n.advertise<std_msgs::Float32MultiArray>("manual_guidance", 1000);
+  ros::Publisher pubDir = n.advertise<std_msgs::Float32>("UpDown", 1000);
 
-  ros::spin();
+  std_msgs::Float32MultiArray controlData = std_msgs::Float32MultiArray();
+  std_msgs::Float32 val = std_msgs::Float32();
+
+  while (ros::ok()) {
+    controlData.data=controls;
+    pub.publish(controlData);
+    
+    if( val.data != controls[4] ) {
+      val.data=controls[4];
+      pubDir.publish(val);
+    }
+    ros::spinOnce();
+  }
 
   return 0;
 }
